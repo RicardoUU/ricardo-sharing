567be66ef482440c47a57a905cf9b29e57ddb1c8
RicardoUU <RicardoUU@users.noreply.github.com>
2022-06-07 15:36:28 +0000

    Auto deploy from Github Actions

---
+++ assets/pages_zhuixu_1.md.d057e767.js
@@ -0,0 +1,38 @@
+import{_ as s,c as n,o as a,d as t}from"./app.6f48252e.js";const g='{"title":"\u8D58\u5A7F\u6768\u8FB0 \u7B2C\u4E00\u7AE0","description":"","frontmatter":{},"headers":[{"level":2,"title":"vitepress \u914D\u7F6E\u7ED3\u675F","slug":"vitepress-\u914D\u7F6E\u7ED3\u675F"},{"level":2,"title":"\u6D4B\u8BD5\u4E0B","slug":"\u6D4B\u8BD5\u4E0B"},{"level":3,"title":"\u6D4B\u8BD5\u4E0B","slug":"\u6D4B\u8BD5\u4E0B-1"}],"relativePath":"pages/zhuixu/1.md"}',e={},p=t(`<h1 id="\u8D58\u5A7F\u6768\u8FB0-\u7B2C\u4E00\u7AE0" tabindex="-1">\u8D58\u5A7F\u6768\u8FB0 \u7B2C\u4E00\u7AE0 <a class="header-anchor" href="#\u8D58\u5A7F\u6768\u8FB0-\u7B2C\u4E00\u7AE0" aria-hidden="true">#</a></h1><h2 id="vitepress-\u914D\u7F6E\u7ED3\u675F" tabindex="-1">vitepress \u914D\u7F6E\u7ED3\u675F <a class="header-anchor" href="#vitepress-\u914D\u7F6E\u7ED3\u675F" aria-hidden="true">#</a></h2><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>This is a tip</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>This is a warning</p></div><div class="danger custom-block"><p class="custom-block-title">WARNING</p><p>This is a dangerous warning</p></div><div class="language-md"><pre><code>::: tip
+This is a tip
+:::
+
+::: warning
+This is a warning
+:::
+
+::: danger
+This is a dangerous warning
+:::
+::: tip
+ \u6700\u540E\u8BF4\u660E\u4E00\u4E0B <span class="token code-snippet code keyword">\`yarn docs:build\`</span> \u540E\u751F\u6210\u7684\u6587\u4EF6\u5728 <span class="token code-snippet code keyword">\`.vitepress\`</span> \u6587\u4EF6\u5939dist\u5185
+:::
+
+</code></pre></div><h2 id="\u6D4B\u8BD5\u4E0B" tabindex="-1">\u6D4B\u8BD5\u4E0B <a class="header-anchor" href="#\u6D4B\u8BD5\u4E0B" aria-hidden="true">#</a></h2><div class="language-js"><div class="highlight-lines"><div class="highlighted">\xA0</div><br><br><div class="highlighted">\xA0</div><br><div class="highlighted">\xA0</div><div class="highlighted">\xA0</div><div class="highlighted">\xA0</div><br><br><br><br></div><pre><code><span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token punctuation">{</span> <span class="token comment">// Highlighted</span>
+  <span class="token function">data</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token punctuation">{</span>
+      <span class="token literal-property property">msg</span><span class="token operator">:</span> <span class="token template-string"><span class="token template-punctuation string">\`</span><span class="token string">Highlighted!
+      This line isn&#39;t highlighted,
+      but this and the next 2 are.</span><span class="token template-punctuation string">\`</span></span><span class="token punctuation">,</span>
+      <span class="token literal-property property">motd</span><span class="token operator">:</span> <span class="token string">&#39;VitePress is awesome&#39;</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">lorem</span><span class="token operator">:</span> <span class="token string">&#39;ipsum&#39;</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div><h3 id="\u6D4B\u8BD5\u4E0B-1" tabindex="-1">\u6D4B\u8BD5\u4E0B <a class="header-anchor" href="#\u6D4B\u8BD5\u4E0B-1" aria-hidden="true">#</a></h3><div class="language-js"><div class="highlight-lines"><div class="highlighted">\xA0</div><br><br><div class="highlighted">\xA0</div><br><div class="highlighted">\xA0</div><div class="highlighted">\xA0</div><div class="highlighted">\xA0</div><br><br><br><br></div><pre><code><span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token punctuation">{</span> <span class="token comment">// Highlighted</span>
+  <span class="token function">data</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token punctuation">{</span>
+      <span class="token literal-property property">msg</span><span class="token operator">:</span> <span class="token template-string"><span class="token template-punctuation string">\`</span><span class="token string">Highlighted!
+      This line isn&#39;t highlighted,
+      but this and the next 2 are.</span><span class="token template-punctuation string">\`</span></span><span class="token punctuation">,</span>
+      <span class="token literal-property property">motd</span><span class="token operator">:</span> <span class="token string">&#39;VitePress is awesome&#39;</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">lorem</span><span class="token operator">:</span> <span class="token string">&#39;ipsum&#39;</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,10),i=[p];function o(l,c,r,d,h,u){return a(),n("div",null,i)}var v=s(e,[["render",o]]);export{g as __pageData,v as default};
